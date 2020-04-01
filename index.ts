d243558fa78ade2fa1ce75974580b6c87c03bf3b
jamc96 <jamc.mejia@gmail.com>
2020-04-01 11:28:19 -0600

    accessing database methods

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
 import { DatabaseFactory } from './Database/factory';
   
-
+// setting database configs
 var dbFactory = new DatabaseFactory();
-
-// couchabase db
-var dbCouchbase = dbFactory.set('mysql','127.0.0.1','myDB');
+var dbCouchbase = dbFactory.set('couchbase','127.0.0.1','myDB');
+var dbMysql = dbFactory.set('mysql','127.0.0.1','myDB');
+// accesing public couchbase methods(connect, save)
+dbCouchbase.connect();
+dbCouchbase.save();
+// accesing public mysql methods(connect, save, find)
+dbMysql.connect();
+dbMysql.save();
+dbMysql.find();
 
 
 
